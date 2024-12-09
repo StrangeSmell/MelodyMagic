0d000d5a8c713dc604be7f3b1244b9e3982ebf7e
StrangeSmell <107765508+StrangeSmell@users.noreply.github.com>
2024-12-10 01:03:19 +0800

    composition workbench 2

--- src/main/java/com/strangesmell/melodymagic/MelodyMagic.java
+++ src/main/java/com/strangesmell/melodymagic/MelodyMagic.java
@@ -3,16 +3,21 @@ package com.strangesmell.melodymagic;
 import com.mojang.serialization.Codec;
 import com.strangesmell.melodymagic.api.MMCriterionTrigger;
 import com.strangesmell.melodymagic.api.SoundEffect;
-import com.strangesmell.melodymagic.block.FakeNetherPortal;
-import com.strangesmell.melodymagic.block.FakeNetherPortalBlockEntity;
-import com.strangesmell.melodymagic.block.SoundPlayerBlock;
-import com.strangesmell.melodymagic.block.SoundPlayerBlockEntity;
+import com.strangesmell.melodymagic.block.*;
 import com.strangesmell.melodymagic.container.ChestConatiner;
 import com.strangesmell.melodymagic.container.WandMenu;
 import com.strangesmell.melodymagic.entity.FriendlyVex;
+import com.strangesmell.melodymagic.hud.DanceHud;
 import com.strangesmell.melodymagic.hud.RecordHud;
 import com.strangesmell.melodymagic.hud.SelectHud;
 import com.strangesmell.melodymagic.item.*;
+import com.sun.jna.Library;
+import com.sun.jna.Memory;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.ptr.IntByReference;
+import com.sun.jna.ptr.PointerByReference;
 import net.minecraft.advancements.CriterionTrigger;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.effect.MobEffects;
@@ -30,7 +35,11 @@ import net.minecraft.world.level.material.PushReaction;
 import net.neoforged.neoforge.attachment.AttachmentType;
 import net.neoforged.neoforge.client.event.RegisterGuiLayersEvent;
 import net.neoforged.neoforge.client.event.sound.SoundEngineLoadEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.neoforge.event.server.ServerStoppingEvent;
 import net.neoforged.neoforge.registries.*;
+import org.jtransforms.fft.FloatFFT_1D;
+import org.lwjgl.BufferUtils;
 import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -47,10 +56,13 @@ import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
+import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Supplier;
 
 import static net.minecraft.resources.ResourceLocation.fromNamespaceAndPath;
@@ -65,6 +77,33 @@ public class MelodyMagic {
     public static Map<String, SoundEffect> KEY2EFFECT = new HashMap<>();
     public static Map<String, Integer> SOUND_INF = new HashMap<>();
     public static Map<String, List<Object>> EFFECT_INF = new HashMap<>();
+
+    /*
+
+
+    public static ByteBuffer PcmData= BufferUtils.createByteBuffer(1024);
+    public static ExecutorService getPcmExecutor = Executors.newSingleThreadExecutor();
+    public static ExecutorService getPcmExecutor2 = Executors.newSingleThreadExecutor();
+    public static ExecutorService getPcmExecutor3 = Executors.newSingleThreadExecutor();
+    public static ExecutorService getPcmExecutor4 = Executors.newSingleThreadExecutor();
+    public static ExecutorService fftExecutor = Executors.newSingleThreadExecutor();
+    public static boolean doFFT = false;
+    public static long intSize = Native.getNativeSize(int.class);
+    public static long pointerSize = Native.getNativeSize(Pointer.class);
+    public static float[][] pcmData = new float[128][];
+    public static boolean[] pcmDataCanRender = new boolean[128];
+    public static int currentSize=0;
+    public static long arraySize=128;
+    //指向指针数组的指针
+    public static PointerByReference bigArr = new PointerByReference();
+    //指针数组大小
+    public static IntByReference size = new IntByReference();
+    //指针数组中每一个数组的大小
+    public static IntByReference smallArrSize = new IntByReference();
+    //是否继续监听
+    public static WinDef.BOOLByReference flag = new WinDef.BOOLByReference();
+*/
+
 
     public static final Logger LOGGER = LogUtils.getLogger();
 
@@ -99,6 +138,10 @@ public class MelodyMagic {
     public static final DeferredBlock<Block> POT_MORNING_GLORY = BLOCKS.register("pot_morning_glory", () -> new FlowerPotBlock(
             () -> ((FlowerPotBlock) Blocks.FLOWER_POT), MORNING_GLORY, BlockBehaviour.Properties.ofFullCopy(Blocks.POTTED_ALLIUM))
     );
+    public static final DeferredBlock<Block> COMPOSITION_WORKBENCH = BLOCKS.register("composition_workbench", () -> new CompositionWorkbenchBlock(
+            BlockBehaviour.Properties.ofFullCopy(Blocks.CRAFTING_TABLE))
+    );
+
 
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(Registries.ENTITY_TYPE, MODID);
     public static final Supplier<EntityType<FriendlyVex>> FRIENDLY_VEX = ENTITIES.register("friendly_vex", () ->
@@ -118,10 +161,11 @@ public class MelodyMagic {
     public static final DeferredItem<CollectionItem> COLLECTION_ITEM = ITEMS.registerItem("collection", CollectionItem::new, new Item.Properties().stacksTo(1));
     public static final DeferredItem<RecordBook> RECORD_BOOK = ITEMS.registerItem("record_book", RecordBook::new, new Item.Properties().stacksTo(1));
     public static final DeferredItem<Item> COLLECTION_DISPLAY_ITEM = ITEMS.registerItem("collection_display", Item::new, new Item.Properties().stacksTo(1));
-    public static final DeferredItem<SoundContainerItem> SOUND_CONTAINER_ITEM = ITEMS.registerItem("sound_container", SoundContainerItem::new, new Item.Properties().rarity(Rarity.RARE).stacksTo(1));
     public static final DeferredItem<ContinueSoundContainerItem> CONTINUE_SOUND_CONTAINER_ITEM = ITEMS.registerItem("continue_sound_container", ContinueSoundContainerItem::new, new Item.Properties().rarity(Rarity.RARE).stacksTo(1));
     public static final DeferredItem<BlockItem> SOUND_PLAYER_ITEM = ITEMS.registerSimpleBlockItem("sound_player_block", SOUND_PLAYER_BLOCK);
     public static final DeferredItem<BlockItem> MORNING_GLORY_ITEM = ITEMS.registerSimpleBlockItem("morning_glory", MORNING_GLORY);
+    public static final DeferredItem<SoundContainerItem> SOUND_CONTAINER_ITEM = ITEMS.registerItem("sound_container", SoundContainerItem::new, new Item.Properties().rarity(Rarity.RARE).stacksTo(1));
+    public static final DeferredItem<BlockItem> COMPOSITION_WORKBENCH_ITEM = ITEMS.registerSimpleBlockItem("composition_workbench", COMPOSITION_WORKBENCH);
 
 
     public static final DeferredRegister<MenuType<?>> MENU_TYPE = DeferredRegister.create(Registries.MENU, MODID);
@@ -136,6 +180,7 @@ public class MelodyMagic {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY = DeferredRegister.create(Registries.BLOCK_ENTITY_TYPE, MODID);
     public static final Supplier<BlockEntityType<SoundPlayerBlockEntity>> SOUND_PLAYER_BLOCK_ENTITY = BLOCK_ENTITY.register("sound_player_block_entity", () -> BlockEntityType.Builder.of(SoundPlayerBlockEntity::new, SOUND_PLAYER_BLOCK.get()).build(null));
     public static final Supplier<BlockEntityType<FakeNetherPortalBlockEntity>> FAKE_NETHER_PORTAL_BLOCK_ENTITY = BLOCK_ENTITY.register("fake_nether_portal_block_entity", () -> BlockEntityType.Builder.of(FakeNetherPortalBlockEntity::new, FAKE_NETHER_PORTAL.get()).build(null));
+    public static final Supplier<BlockEntityType<CompositionWorkbenchBlockEntity>> COMPOSITION_WORKBENCH_BLOCK_ENTITY = BLOCK_ENTITY.register("composition_workbench_block_entity", () -> BlockEntityType.Builder.of(CompositionWorkbenchBlockEntity::new, COMPOSITION_WORKBENCH.get()).build(null));
 
     private static final DeferredRegister<AttachmentType<?>> ATTACHMENT_TYPES = DeferredRegister.create(NeoForgeRegistries.ATTACHMENT_TYPES, MODID);
     // Serialization via codec
@@ -160,6 +205,7 @@ public class MelodyMagic {
                 output.accept(SOUND_PLAYER_ITEM.get());
                 output.accept(RECORD_BOOK.get());
                 output.accept(MORNING_GLORY_ITEM.get());
+                output.accept(COMPOSITION_WORKBENCH_ITEM.get());
             }).build());
 
 
@@ -177,11 +223,13 @@ public class MelodyMagic {
         ATTACHMENT_TYPES.register(modEventBus);
 
         NeoForge.EVENT_BUS.register(this);
-        modEventBus.addListener(this::addCreative);
 
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
 
         init.init();
+
+
+
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
@@ -198,25 +246,48 @@ public class MelodyMagic {
         //Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-
-        }
-
-    }
-
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
         // Do something when the server starts
         LOGGER.info("HELLO from server starting");
     }
+/*    @SubscribeEvent
+    public void onServerStopping(ServerStoppingEvent event) {
+        // Do something when the server starts
+        Native.free(Pointer.nativeValue(bigArr.getPointer()));
+        Native.free(Pointer.nativeValue(size.getPointer()));
+        Native.free(Pointer.nativeValue(smallArrSize.getPointer()));
+        Native.free(Pointer.nativeValue(flag.getPointer()));
+
+    }*/
+/*
+    @SubscribeEvent
+    public void playerLoggedOutEvent(PlayerEvent.PlayerLoggedOutEvent event) {
+        if( event.getEntity().level().isClientSide){
+            // Do something when the server starts
+            getPcmExecutor.close();
+            fftExecutor.close();
+            Native.free(Pointer.nativeValue(bigArr.getPointer()));
+            Native.free(Pointer.nativeValue(size.getPointer()));
+            Native.free(Pointer.nativeValue(smallArrSize.getPointer()));
+            Native.free(Pointer.nativeValue(flag.getPointer()));
+        }
+
+
+
+    }
+*/
 
 
     @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
-        @SubscribeEvent
+/*        @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
-        }
+            System.out.println("star get pcm");
+            getPcmExecutor.submit(MelodyMagic::getPcm);
+
+            fftExecutor.submit(MelodyMagic::fftPcm);
+        }*/
 
         @SubscribeEvent
         public static void loadSoundEngine(SoundEngineLoadEvent event) {
@@ -229,8 +300,119 @@ public class MelodyMagic {
             LOGGER.info("Register hud");
             event.registerAboveAll(fromNamespaceAndPath(MODID, "select_hud"), SelectHud.getInstance());
             event.registerAboveAll(fromNamespaceAndPath(MODID, "record_hud"), RecordHud.getInstance());
+
+            event.registerAboveAll(fromNamespaceAndPath(MODID, "dance_hud"), DanceHud.getInstance());
         }
     }
 
+/*    public interface Audio2 extends Library {
+        //Audio2 INSTANCE = Native.load("audioDllX6410OUT100", Audio2.class);
+        //Audio2 INSTANCE = Native.load("audioDllX64OUT100AndSleep100", Audio2.class);
+        //Audio2 INSTANCE = Native.load("audioDllX640Sleep100", Audio2.class);
+        //Audio2 INSTANCE = Native.load("audioDllOutTime", Audio2.class);
+        //Audio2 INSTANCE = Native.load("audioDllX645", Audio2.class);
+        Audio2 INSTANCE = Native.load("audioDllX6410WithOut", Audio2.class);
+        //Audio2 INSTANCE = Native.load("audioDllX6410HM", Audio2.class);
+        int add(int a,int b);
+        void getIntArr(PointerByReference bigArr, IntByReference size, IntByReference smallArrSize, WinDef.BOOLByReference smallArr);
+        //void cleanIntArr(Pointer arr);
+    }*/
+
+
+/*
+    public static void fftPcm(){
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        while(true){
+            while (doFFT){
+                //获取指针数组对应指针指向的数组是否有值，没有值则等待数组
+                //System.out.print("fft");
+                int currentArrSize=0;
+                try {
+                    currentArrSize = smallArrSize.getPointer().getInt(currentSize*intSize);
+                } catch (java.lang.Error e) {
+                    continue;
+                }
+                //System.out.println("currentArrSize:"+currentArrSize);
+                if (currentArrSize > 0) {
+
+                    //有值则从bigArr中获取对应指针数组的指针
+                    Pointer smallArrPointer = bigArr.getPointer().getPointer(currentSize*pointerSize);
+
+                    float max =1;
+                    pcmData[currentSize] = new float[currentArrSize];
+                    for (int i = 0; i < currentArrSize; i++) {
+                        pcmData[currentSize][i] = smallArrPointer.getFloat(i*intSize);
+                        max=Math.max(max,Math.abs(pcmData[currentSize][i]));
+                        //System.out.println(smallArrPointer.getFloat(i*intSize) + " i " + i);
+                    }
+                    //归一化
+                    for (int i = 0; i < currentArrSize; i++) {
+                        pcmData[currentSize][i] = pcmData[currentSize][i]/max;
+                    }
+                    FloatFFT_1D fft = new FloatFFT_1D(arraySize);
+                    fft.realForward(pcmData[currentSize]);
+                    //读完值后且傅里叶变换完成
+                    //System.out.println("fft over");
+                    pcmDataCanRender[currentSize] = true;
+                    //读取完后重新设置为0
+                    smallArrSize.getPointer().setInt(currentSize*intSize, 0);
+                    currentSize++;
+                    //循环读取
+                    if (currentSize >= arraySize) currentSize = currentSize - (int) arraySize;
+                }
+            }
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }*/
+
+/*
+
+    public static void getPcm(){
+        long intSize = Native.getNativeSize(int.class);
+        long pointerSize = Native.getNativeSize(Pointer.class);
+        try (Memory bigArrMemory = new Memory(arraySize * pointerSize)) {
+            bigArr.setPointer(bigArrMemory);
+            try (Memory smallArrSizeMemory = new Memory(arraySize * intSize)) {
+                smallArrSize.setPointer(smallArrSizeMemory);
+                for(int m=0;m<arraySize;m++) {
+                    smallArrSize.getPointer().setInt(m,0);
+                }
+                try (Memory sizeMemory = new Memory(intSize)) {
+                    size.setPointer(sizeMemory);
+                    size.setValue((int) arraySize);
+                    try (Memory flagMemory = new Memory( intSize)) {
+                        flag.setPointer(flagMemory);
+                        flag.setValue(new WinDef.BOOL(true));
+                        //开始读取pcm
+                        doFFT=true;
+                        long startTime = System.nanoTime();
+
+                        MelodyMagic.Audio2.INSTANCE.getIntArr(bigArr, size,smallArrSize,flag);
+
+                        long endTime = System.nanoTime();
+                        long duration = endTime - startTime;
+                        System.out.println("time :" + duration + "nm");
+
+                    } catch (Exception e) {
+                        System.out.print("FlagMemory Error");
+                    }
+                } catch (Exception e) {
+                    System.out.print("SizeMemory Error");
+                }
+            } catch (Exception e) {
+                System.out.print("SmallArrSizeMemory Error");
+            }
+        } catch (Exception e) {
+            System.out.print("Arr Error");
+        }
+    }*/
 
 }
